a4d54cbde2b285d7502289cc04c341dbfa8020da
SrabanMondal <152634742+SrabanMondal@users.noreply.github.com>
2025-01-02 16:24:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ app.get("/",(req,res)=>{
 })
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin:'*',
+    methods: "GET, POST, PUT, DELETE, PATCH"  
+}))
+
 app.use(express.urlencoded({ extended: true }))
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 app.use("/api/v1/admin",adminRouter);
 export default app;
